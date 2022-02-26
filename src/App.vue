ba3d8ed37c56c54556590cd007b918b52f0bf6d3
daniyal <daniyal.sfr@gmail.com>
2022-02-26 09:44:33 +0330

    add offline message

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,10 @@
 <script>
 export default {
   name : "App",
-  data(){
-    return{
-      onLine: navigator.onLine,
-      showBackOnline: false
-    }
-  },
-  methods:{
-    updateOnlineStatus(e) {
-      this.onLine = e.type === 'online';
-    }
-  },
-  watch :{
-    onLine(v) {
-      if (v) {
-        this.showBackOnline = true;
-        setTimeout(() => {
-          this.$toast.error('اینترنت خود را بررسی کنید')
-          this.showBackOnline = false;
-        }, 1000);
-      }
-    }
-  },
-  mounted() {
-    window.addEventListener('online', this.updateOnlineStatus);
-    window.addEventListener('offline', this.updateOnlineStatus);
-  },
-  beforeDestroy() {
-    window.removeEventListener('online', this.updateOnlineStatus);
-    window.removeEventListener('offline', this.updateOnlineStatus);
+  mounted () {
+    this.$on('offline', () => {
+      this.$toast.error('خطا !! اینترنت خود را بررسی کنید')
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import store from './store'
 import './assets/scss/app.scss'
 import VueToast from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
+import VueOffline from 'vue-offline'
 
+Vue.use(VueOffline)
 Vue.config.productionTip = false
 
 Vue.use(VueToast,{
